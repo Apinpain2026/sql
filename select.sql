2fbadaa041cb7962fcd8d2a5e41c9095c7206b70
Apinpain2026 <apin2.2026@gmail.com>
2025-06-28 15:06:40 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,11 @@
+select  *
+from parks_and_recreation.employee_demographics;
+select first_name,
+age,
+age+10
+
+ from employee_demographics;
+#pemdas
+#parenthsis exponential multiplication divison add sub
+select distinct first_name,age
+from employee_demographics; 
